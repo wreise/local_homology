406a3337034362d7a68485991c552c6087e18203
wreise <wojciech.reise@epfl.ch>
2022-02-18 17:02:31 +0100

    New filtration; add docs; rename stuff from the alpha_filtration

---
+++ local_homology/alpha_filtration.py
@@ -0,0 +1,87 @@
+import numpy as np
+
+from sklearn.metrics import pairwise_distances
+
+from gudhi import RipsComplex, SimplexTree
+
+
+## Coning Variant
+def compute_local_homology_alpha(point_cloud, x0, epsilon, max_dimension):
+    """Compute the local homology of point_cloud at x0, with a pseudo alpha-filtration.
+
+    """
+    st = build_local_complex_alpha(point_cloud, x0, epsilon, max_dimension + 1)
+    pd = get_persistence(st, max_dimension)
+    return pd
+
+
+def is_point_in_ball(point_cloud, point, epsilon, return_distances=True):
+    """Determine which points from point_cloud are in the epsilon-ball centered at point.
+    :param point_cloud: coordinates of points.
+    :type point_cloud: np.ndarray of size (n_pts, dim)
+    :param point: center of the ball that defines the neighborhood, does not have to be in point_cloud.
+    :type point: np.ndarray of shape (1, dim)
+    :param epsilon: size of the neighborhood to consider
+    :type epsilon: float, positive
+    :param return_distances: return distances if True
+    :type return_distances: bool
+    
+    :returns: arrays specifying whether a given ball is, or not, in the complex.
+    :
+    """
+    distance_from_point =  pairwise_distances(point_cloud, point, metric="euclidean")
+    distance_to_boundary = epsilon - distance_from_point
+    is_in_ball = (distance_to_boundary >= 0)[:,0]
+    if return_distances:
+        return is_in_ball, distance_to_boundary
+    return is_in_ball
+
+
+def build_local_complex_alpha(point_cloud, point, epsilon, max_dimension):
+    """Build a simplicial complex on vertices from
+    point_cloud \cap B(point, \epsilon), along with an extra vertex
+    representing the boundary.
+    :param point_cloud: coordinates of points.
+    :type point_cloud: np.ndarray of size (n_pts, dim)
+    :param point: center of the ball that defines the neighborhood,
+        does not have to be in point_cloud.
+    :type point: np.ndarray of shape (1, dim)
+    :param epsilon: size of the neighborhood to consider
+    :type epsilon: float, positive
+    :param max_dimension: max dimension to compute the homology of.
+    """
+    point_cloud_dist = pairwise_distances(point_cloud, metric="euclidean")
+    is_in_ball, distance_to_boundary = is_point_in_ball(point_cloud, point,
+                                                        epsilon, return_distances=True)
+    
+    point_cloud_local = point_cloud[is_in_ball]
+    point_cloud_dist_local = point_cloud_dist[is_in_ball][:, is_in_ball]
+    local_boundary = distance_to_boundary[is_in_ball]
+
+    point_cloud_local_stared = np.concatenate([point_cloud_dist_local/2., local_boundary.T],
+                                              axis=0)
+    point_cloud_local_stared = np.concatenate([point_cloud_local_stared,
+                                               np.concatenate([local_boundary, np.array([[0]])],
+                                                              axis=0)], axis=1)
+    
+    rc = RipsComplex(distance_matrix=point_cloud_local_stared)
+    st = rc.create_simplex_tree(max_dimension=max_dimension)
+    n_points_in_ball = np.sum(is_in_ball)
+    star_vertex = n_points_in_ball  # indexing starts at 0!
+    st.assign_filtration([star_vertex], -1)
+    return st
+
+
+def get_persistence(simplexTree, max_dimension):
+    """Calculate the persistent homology of the abstract simplicial complex,
+    filtering by positive values and dimensions.
+    :param simplexTree: a simplcial complex, as returned by `build_local_complex`
+    :type simplexTree: simplexTree
+    :param max_dimension: max dimension of persistent homology to be returned.
+    :type max_dimension: int.
+    
+    :returns: persistence diagram in the Gudhi format.
+    :rtype: list of tuples (dim, (birth, death)).
+    """
+    pd = simplexTree.persistence()
+    return [p for p in pd if (p[0] <= max_dimension) & (p[1][0] >= 0.)]
---
+++ local_homology/r_filtration.py
@@ -0,0 +1,108 @@
+from typing import List, Tuple
+
+import gudhi.simplex_tree
+import numpy as np
+from gudhi import SimplexTree
+from matplotlib import pyplot as plt
+
+
+def compute_local_homology_r(point_cloud: np.ndarray, x0: np.ndarray, alpha: float,
+                             max_dimension: int) -> List[Tuple]:
+    """Calculate the local persistent homology of `point_cloud` at point `x_0`,
+    with scale parameter alpha. Computed using the approximation technique
+    from [1]. **Note:** the minimal dimension with non-trivial
+    features is 1.
+    :param point_cloud: the point-cloud to compute the homology of.
+    :type point_cloud:  ndarray of shape (n_points, d).
+    :param x0: the point at which to compute the local homology.
+    :type x0: ndarray of shape (1, d).
+    :param alpha: construct a Rips with 2*alpha.
+    :type alpha: positive float.
+    :param max_dimension: max dimension of homology that you want to see.
+    :type max_dimension: int
+
+    :returns: list of tuples (dim, (b,d)) of the persistence diagram.
+
+    1. Skraba, P. & Wang, B. Approximating Local Homology from Samples.
+       In Proceedings of the Twenty-Fifth Annual ACM-SIAM Symposium on Discrete
+       Algorithms 174–192 (Society for Industrial and Applied Mathematics, 2014).
+       doi:10.1137/1.9781611973402.13.
+    """
+    St = get_filtered_complex(point_cloud, x0, alpha, max_dimension)
+    # For H_k, by corollary, it is H_{k-1}, so you need to build up to dim=k.
+    St.extend_filtration()
+
+    dgm_ordinary_minus, _, _, _ = St.extended_persistence()
+    dgm_relative = apply_duality_ordinary_to_relative(dgm_ordinary_minus)
+
+    return dgm_relative
+
+
+def get_filtered_complex(point_cloud: np.ndarray, x0: np.ndarray, alpha: float,
+                         max_dimension: int) -> gudhi.simplex_tree.SimplexTree:
+    """Construct the VietorisRips complex from `point_cloud` with scale alpha.
+    Filter by the function $x \mapsto - d(x, x_0)$ and expand until
+    dimension max_dimension.
+
+    Params:
+    -------
+        point_cloud: ndarray of shape (n_points, d). The point-cloud to build
+            the complex of.
+        x0: ndarray of shape (1, d). The point at which to compute the local
+            homology.
+        alpha: positive float. Construct a Rips with 2*alpha.
+        max_dimension: int. Maximal simplex dimension.
+    Returns:
+    --------
+        St: gudhi.SimplexTree
+    """
+    dist_to_x0 = np.linalg.norm(point_cloud - x0, ord=2, axis=1)
+    vertex_value = -dist_to_x0
+    pairwise_distances = np.linalg.norm(point_cloud[None, :, :] - point_cloud[:, None, :],
+                                        ord=2, axis=-1)
+
+    is_edge_in_complex = pairwise_distances < alpha
+
+    St = SimplexTree()
+    for i1, val in enumerate(vertex_value):
+        St.insert([i1], val)
+
+    for (i1, i2), is_in_complex in np.ndenumerate(is_edge_in_complex):
+        if is_in_complex:
+            edge_value = max(vertex_value[i1], vertex_value[i2])
+            St.insert([i1, i2], edge_value)
+
+    St.expansion(max_dimension)
+    return St
+
+
+def apply_duality_ordinary_to_relative(dgm_ordinary):
+    """Apply the Symmetry Corollary, transforming the ordinary diagram of $-f$
+    to the relative diagram of $f$.
+
+    Params:
+    -------
+        dgm_ordinary: list of tuples (dim, (b,d)) representing a persistence diagram.
+    Returns:
+    --------
+        dgm_relative: list of tuples (dim, (b,d)) representing a persistence diagram.
+    """
+    dgm_relative = [(dim + 1, (-d if np.isfinite(d) else 0, -b)) for dim, (b, d) in dgm_ordinary]
+    return dgm_relative
+
+
+def plot_one_skeleton(X, x0, alpha):
+    """Visualize the 1-skeleton of the VietorisRips complex built on `X`.
+    """
+    St = get_filtered_complex(X, x0, alpha, 1)
+
+    simplices = [simplex for simplex, simplex_v in St.get_filtration() if (len(simplex) == 2)]
+    n_points = X.shape[0]
+    for simplex in simplices:
+        i_a, i_b = simplex
+        if (i_a < n_points) & (i_b < n_points):
+            plt.plot([X[i_a][0], X[i_b][0]],
+                     [X[i_a][1], X[i_b][1]], c='grey', alpha=0.3)
+    ax = plt.gca()
+    plt.scatter(X[:, 0], X[:, 1])
+    ax.add_patch(plt.Circle(x0[0], alpha, fill=False))
